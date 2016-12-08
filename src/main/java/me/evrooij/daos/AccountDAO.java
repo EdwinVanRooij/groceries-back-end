99156c4674640aa0d87529a977d8522f42af9b01
Edwin <edwinvrooij@gmail.com>
2016-12-08 22:49:04 +0100

    fixed #4, huge commit, ready to unit test it now

---
+++ src/main/java/me/evrooij/daos/AccountDAO.java
@@ -0,0 +1,51 @@
+package me.evrooij.daos;
+
+import me.evrooij.domain.Account;
+
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import javax.persistence.*;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+/**
+ * @author eddy on 8-12-16.
+ */
+@NamedQueries({
+        @NamedQuery(
+                name = "Account.findByUsernameAndPassword",
+                query = "SELECT a FROM Account a WHERE a.username = :username AND a.password = :password"),
+})
+public class AccountDAO {
+    private final EntityManagerFactory entityManagerFactory;
+    private final EntityManager entityManager;
+
+    public AccountDAO() {
+        entityManagerFactory = Persistence.createEntityManagerFactory("NewPersistenceUnit");
+        entityManager = entityManagerFactory.createEntityManager();
+    }
+
+    public Account register(String username, String email, String password) {
+        Account account = new Account(username, email, password);
+        entityManager.getTransaction().begin();
+        entityManager.persist(account);
+        entityManager.getTransaction().commit();
+        entityManager.close();
+        return account;
+    }
+
+    public Account getAccount(String username, String password) {
+        Session session = entityManager.unwrap(Session.class);
+        entityManager.getTransaction().begin();
+        Query query = session.getNamedQuery("Account.findByUsernameAndPassword");
+        query.setString("username", username);
+        query.setString("password", password);
+        return (Account) query.uniqueResult();
+    }
+}
--- src/main/java/me/evrooij/managers/AccountManager.java
+++ src/main/java/me/evrooij/managers/AccountManager.java
@@ -1,72 +1,29 @@
 package me.evrooij.managers;
 
+import me.evrooij.daos.AccountDAO;
 import me.evrooij.domain.Account;
-import me.evrooij.errors.InvalidAccountException;
-
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import java.util.ArrayList;
-import java.util.List;
 
 public class AccountManager {
 
-    EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("NewPersistenceUnit");
-
-    // temporary, will get from database later on
-    private List<Account> accounts;
+    private AccountDAO accountDAO;
 
     public AccountManager() {
-        accounts = new ArrayList<>();
+        accountDAO = new AccountDAO();
     }
 
-    public Account getAccount(String username, String password) throws InvalidAccountException {
-        failIfInvalid(username, password);
-
-        // todo: get account from database
-        for (Account account :
-                accounts) {
-            if (account.getUsername().equals(username) && account.getPassword().equals(password)) {
-                return account;
-            }
+    public Account getAccount(String username, String password) {
+        if (username == null || username.isEmpty()) {
+            throw new IllegalArgumentException("Parameter 'username' cannot be empty");
         }
-        throw new InvalidAccountException("No account found with this username/password combination");
-    }
-
-    public Account registerAccount(String username, String email, String password) throws InvalidAccountException {
-        failIfInvalid(username, email, password);
-
-        Account account = new Account(username, email, password);
-        // todo: persist account to database
-
-        accounts.add(account);
-        return account;
-    }
-
-    /**
-     * Example of save to database, should actually work already lmao, clean up tho
-     *
-     * @param account
-     */
-    public void saveToDatabase(Account account) {
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        entityManager.getTransaction().begin();
-        entityManager.persist(account);
-        entityManager.getTransaction().commit();
-        entityManager.close();
-    }
-
-    private void failIfInvalid(String name, String email) throws IllegalArgumentException {
-        if (name == null || name.isEmpty()) {
-            throw new IllegalArgumentException("Parameter 'name' cannot be empty");
+        if (password == null || password.isEmpty()) {
+            throw new IllegalArgumentException("Parameter 'password' cannot be empty");
         }
-        if (email == null || email.isEmpty()) {
-            throw new IllegalArgumentException("Parameter 'email' cannot be empty");
-        }
+
+        return accountDAO.getAccount(username, password);
     }
 
-    private void failIfInvalid(String name, String email, String password) throws IllegalArgumentException {
-        if (name == null || name.isEmpty()) {
+    public Account registerAccount(String username, String email, String password) {
+        if (username == null || username.isEmpty()) {
             throw new IllegalArgumentException("Parameter 'name' cannot be empty");
         }
         if (email == null || email.isEmpty()) {
@@ -75,5 +32,7 @@ public class AccountManager {
         if (password == null || password.isEmpty()) {
             throw new IllegalArgumentException("Parameter 'password' cannot be empty");
         }
+
+        return accountDAO.register(username, email, password);
     }
 }
