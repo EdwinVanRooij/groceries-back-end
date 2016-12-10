5cb1e54db93ee099ab553beb0843c48bbad0cc38
Edwin <edwinvrooij@gmail.com>
2016-12-10 16:56:40 +0100

    completed unit tests for getaccount and registeraccount, fixes #8 and fixes #7

--- src/test/java/me/evrooij/managers/AccountManagerTest.java
+++ src/test/java/me/evrooij/managers/AccountManagerTest.java
@@ -1,8 +1,10 @@
 package me.evrooij.managers;
 
 import me.evrooij.domain.Account;
+import me.evrooij.util.DatabaseUtil;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -28,51 +30,82 @@ public class AccountManagerTest {
 
     private AccountManager accountManager;
 
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        new DatabaseUtil().clean();
+    }
+
     @Before
     public void setUp() throws Exception {
         accountManager = new AccountManager();
+    }
 
+    @After
+    public void tearDown() throws Exception {
+        new DatabaseUtil().clean();
     }
 
     @Test
     public void getAccount() throws Exception {
-        /**
-         * Returns an account
-         *
-         * @param username
-         * @param password
-         * @return
+        /*
+         * Register some accounts and verify that we can retrieve them
          */
+
+        // Register
         accountManager.registerAccount(USERNAME_1, EMAIL_1, PASSWORD_1);
 
-        accountManager.registerAccount(USERNAME_1, EMAIL_1, PASSWORD_1);
+        // Get the actual account
+        Account actual_1 = accountManager.getAccount(USERNAME_1, PASSWORD_1);
+        assertNotNull(actual_1);
 
-        accountManager.registerAccount(USERNAME_1, EMAIL_1, PASSWORD_1);
+        // Make sure we can't get an uncreated account
+        Account actual_2 = accountManager.getAccount(USERNAME_2, PASSWORD_2);
+        assertNull(actual_2);
 
+        Account actual_3 = accountManager.getAccount(USERNAME_3, PASSWORD_3);
+        assertNull(actual_3);
     }
 
     @Test
     public void registerAccount() throws Exception {
-        /**
-         * Registers an account
-         *
-         * @param username
-         * @param email
-         * @param password
-         * @return
+        /*
+         * Create accounts and verify their equality
+         * Can't use assertEquals here because we don't know the generated ID for each individual account
          */
 
+        // Register
+        accountManager.registerAccount(USERNAME_1, EMAIL_1, PASSWORD_1);
+
+        // Get the actual account
+        Account actual_1 = accountManager.getAccount(USERNAME_1, PASSWORD_1);
+
+        // Verify it's existence
+        assertNotNull(actual_1);
+
+        // Now some robustness checks, verify creation of 2 more accounts
+        accountManager.registerAccount(USERNAME_2, EMAIL_2, PASSWORD_2);
+        Account actual_2 = accountManager.getAccount(USERNAME_1, PASSWORD_1);
+        assertNotNull(actual_2);
+
+        accountManager.registerAccount(USERNAME_3, EMAIL_3, PASSWORD_3);
+        Account actual_3 = accountManager.getAccount(USERNAME_1, PASSWORD_1);
+        assertNotNull(actual_3);
     }
 
     @Test
     public void removeAccount() throws Exception {
-        /**
-         * Removes an account from the database if the username/password combination is correct
-         *
-         * @param username
-         * @param password
-         * @return boolean value indicating the exit status of removal
+        /*
+         * Remove some existing accounts from the database
          */
 
+        // Register
+        accountManager.registerAccount(USERNAME_1, EMAIL_1, PASSWORD_1);
+        boolean actual_1 = accountManager.removeAccount(USERNAME_1, PASSWORD_1);
+        // Verify that this removal was successful
+        assertTrue(actual_1);
+
+        // Try to remove a non-existent account
+        boolean actual_2 = accountManager.removeAccount(USERNAME_2, PASSWORD_2);
+        assertFalse(actual_2);
     }
 }
