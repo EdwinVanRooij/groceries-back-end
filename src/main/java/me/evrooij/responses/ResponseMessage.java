8eab74d22bac7f41e359863b1b4fc29213956e3f
Edwin <edwinvrooij@gmail.com>
2016-12-11 18:51:04 +0100

    removed unused stuff

--- src/main/java/me/evrooij/responses/ResponseMessage.java
+++ src/main/java/me/evrooij/responses/ResponseMessage.java
@@ -4,16 +4,16 @@ package me.evrooij.errors;
  * @author eddy on 6-12-16.
  *         Small helper class which converts an exception or unformatted string message to a complete string field
  */
-public class ResponseError {
+public class ResponseMessage {
     // Method is used by json converter to show it's message
     @SuppressWarnings("unused")
     private String message;
 
-    public ResponseError(String message, String... args) {
+    public ResponseMessage(String message, String... args) {
         this.message = String.format(message, (Object) args);
     }
 
-    public ResponseError(Exception e) {
+    public ResponseMessage(Exception e) {
         this.message = e.getMessage();
     }
 }
