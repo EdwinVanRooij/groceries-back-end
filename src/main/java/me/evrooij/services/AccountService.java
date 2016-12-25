778e1929dd9bbdf1c941b6e6d0c65758e7e88dac
Edwin <edwinvrooij@outlook.com>
2016-12-25 13:52:11 +0100

    changed service to remove redundant own id

--- src/main/java/me/evrooij/services/AccountService.java
+++ src/main/java/me/evrooij/services/AccountService.java
@@ -36,12 +36,25 @@ public class AccountService extends DefaultService {
             return returning;
         }, json());
 
-        get("/accounts/:accountId/friends/find", (request, response) -> {
-            int accountId = Integer.valueOf(request.params(":accountId"));
-
+        get("/accounts/find", (request, response) -> {
             String searchQuery = request.queryParams("query");
 
             return accountManager.searchFriends(searchQuery);
+        }, json());
+
+        post("/accounts/<account_id>/friends/add", (request, response) -> {
+            String json = request.body();
+            System.out.println(String.format("Received json from /users/register in req body: %s", json));
+
+            Map<String, String> accountMap = new Gson().fromJson(request.body(), Map.class);
+            String username = accountMap.get("username");
+            String email = accountMap.get("email");
+            String password = accountMap.get("password");
+            System.out.println(String.format("Username: %s\nEmail: %s\nPassword: %s", username, email, password));
+
+            Account returning = accountManager.registerAccount(username, email, password);
+            System.out.println(String.format("Returning: %s", returning));
+            return returning;
         }, json());
 
         before(this::beforeRouteHandle);
