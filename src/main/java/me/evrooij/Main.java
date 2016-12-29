1c7a1487ea1ae387bbe6c1f648c2d99ab2e8a28c
Edwin <edwinvrooij@outlook.com>
2016-12-29 20:29:32 +0100

    updated some final touches for production env

--- src/main/java/me/evrooij/Main.java
+++ src/main/java/me/evrooij/Main.java
@@ -6,7 +6,6 @@ import me.evrooij.services.GroceryListService;
 public class Main {
     public static void main(String[] args) {
         // Source: https://dzone.com/articles/building-simple-restful-api
-//        new UserService();
         new AccountService();
         new GroceryListService();
     }
