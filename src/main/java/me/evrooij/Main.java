4493c2640ca78613cd41746f998bd9ea47194c39
Edwin <edwinvrooij@outlook.com>
2017-01-23 21:00:36 +0100

    completed swagger api doc generation

--- src/main/java/me/evrooij/Main.java
+++ src/main/java/me/evrooij/Main.java
@@ -1,18 +1,44 @@
 package me.evrooij;
 
+import io.swagger.annotations.Contact;
+import io.swagger.annotations.Info;
+import io.swagger.annotations.SwaggerDefinition;
+import io.swagger.annotations.Tag;
 import me.evrooij.services.AccountService;
 import me.evrooij.services.FeedbackService;
 import me.evrooij.services.GroceryListService;
 
-import static spark.Spark.port;
+import static spark.Spark.*;
 
+@SwaggerDefinition(host = "localhost:4567",
+        info = @Info(description = "Groceries API",
+                version = "1.0.0",
+                title = "Back-end service for the Groceries application.",
+                contact = @Contact(name = "Edwin", url = "https://evrooij.me/")),
+        schemes = {SwaggerDefinition.Scheme.HTTP},
+        consumes = {"application/json"},
+        produces = {"application/json"},
+        tags = {@Tag(name = "api")})
 public class Main {
-    public static void main(String[] args) {
-        port(Config.getInstance().PORT);
 
-        // Source: https://dzone.com/articles/building-simple-restful-api
-        new AccountService();
-        new GroceryListService();
-        new FeedbackService();
+    public static final String APP_PACKAGE = "me.evrooij";
+
+    public static void main(String[] args) {
+        try {
+            port(Config.getInstance().PORT);
+
+            // Quite unsafe!
+            before(new CorsFilter());
+
+            // Build swagger json description
+            final String swaggerJson = SwaggerParser.getSwaggerJson(APP_PACKAGE);
+            get("/swagger", (req, res) -> swaggerJson);
+
+            new AccountService();
+            new GroceryListService();
+            new FeedbackService();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
